b5e8f2cc8d125a32c0d0ac9dcea8be2781b545e1
Bruno <bcnetbr@gmail.com>
2020-10-09 11:18:24 -0300

    Iniciando o backend do projeto-09-10-20

---
+++ GenCompBackend/src/main/java/com/GenCompBackend/GenCompBackendApplication.java
@@ -0,0 +1,13 @@
+package com.GenCompBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenCompBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenCompBackendApplication.class, args);
+	}
+
+}
