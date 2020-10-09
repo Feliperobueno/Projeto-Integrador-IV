44f65dacf410ea8b033ae8169a28d1c0d7adade2
Bruno <bcnetbr@gmail.com>
2020-10-09 11:27:27 -0300

    Pacotes criados, classe pessoa criada 09-10-20

---
+++ GenCompBackend/src/main/java/com/GenCompBackend/model/Pessoa.java
@@ -0,0 +1,115 @@
+package com.GenCompBackend.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pessoa")
+public class Pessoa {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private Long id;
+	
+	@Column(name="nome")
+	private String nome;
+	
+	@Column(name="cpfcnpj")
+	private String cpfCnpj;
+	
+	@Column(name="telefone")
+	private String telefone;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="funcao")
+	private String funcao;
+	
+	@Column(name="login")
+	private String login;
+	
+	@Column(name="senha")
+	private String senha;
+	
+	@Column(name="perfil")
+	private String perfil;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpfCnpj() {
+		return cpfCnpj;
+	}
+
+	public void setCpfCnpj(String cpfCnpj) {
+		this.cpfCnpj = cpfCnpj;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFuncao() {
+		return funcao;
+	}
+
+	public void setFuncao(String funcao) {
+		this.funcao = funcao;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getPerfil() {
+		return perfil;
+	}
+
+	public void setPerfil(String perfil) {
+		this.perfil = perfil;
+	}
+
+}
