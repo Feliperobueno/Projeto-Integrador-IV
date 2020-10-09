dd606eaefc2d9458655ee45411e8c97b4509ddab
Bruno <bcnetbr@gmail.com>
2020-10-09 10:55:45 -0300

    Backend projeto integrador

---
+++ Backend/GenComp/src/main/java/com/GenComp/GenCompApplication.java
@@ -0,0 +1,13 @@
+package com.GenComp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenCompApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenCompApplication.class, args);
+	}
+
+}
---
+++ Backend/GenComp/src/test/java/com/GenComp/GenCompApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GenComp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenCompApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
