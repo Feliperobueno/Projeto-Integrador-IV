3e6f6523ad8e7a44ad86aeb79c31f4ca72117a62
Bruno <bcnetbr@gmail.com>
2020-10-09 13:54:25 -0300

    classes models criadas 09-10-20

---
+++ GenCompBackend/src/main/java/com/GenCompBackend/model/Historico.java
@@ -0,0 +1,79 @@
+package com.GenCompBackend.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="historico")
+public class Historico {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name="id_ordemservico")
+	private OrdemServico ordemServico;
+
+	@Column(name="data")
+	private Date data;
+	
+	@Column(name="status")
+	private String status;
+	
+	@ManyToOne
+	@JoinColumn(name="id_funcionario")
+	private Pessoa funcionario;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public OrdemServico getOrdemServico() {
+		return ordemServico;
+	}
+
+	public void setOrdemServico(OrdemServico ordemServico) {
+		this.ordemServico = ordemServico;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Pessoa getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(Pessoa funcionario) {
+		this.funcionario = funcionario;
+	}
+	
+	
+
+}
---
+++ GenCompBackend/src/main/java/com/GenCompBackend/model/TipoServico.java
@@ -0,0 +1,40 @@
+package com.GenCompBackend.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tiposervico")
+public class TipoServico {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private Long id;
+	
+	@Column(name="descricao")
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+
+}
